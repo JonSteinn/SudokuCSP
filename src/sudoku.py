fc23b4fd83e40c6fb6b97973ee1ead792892f503
JonSteinn <jonsteinn@gmail.com>
2020-09-05 10:31:52 +0000

    pylintify sudoku.py

--- src/sudoku.py
+++ src/sudoku.py
@@ -7,17 +7,21 @@
 import sys
 from pathlib import Path
 
+
 def read_puzzles(name):
     """
-    Reads in Sudoku puzzle specifications from a file. The puzzles must be separated by one or more separator lines.
-    A separator line is any line starting with a alpha-letter (a-z, A-Z) or the '#' symbol.
-    The format for specifying each puzzle is flexible as long as:
-      - The digits 1-9 specify a filled cell and the letter '.' (or the digit 0) an empty cell.
-        The digits (and '.') are process in a left-to-right and top-to-bottom order, that is, the first digit (or
-        '.') encountered specifies the value of the top-left most cell, the second, the top-second-leftmost, ...,
-        and the last one the bottom-right-most cell.
+    Reads in Sudoku puzzle specifications from a file. The puzzles must be separated by
+    one or more separator lines. A separator line is any line starting with a
+    alpha-letter (a-z, A-Z) or the '#' symbol. The format for specifying each puzzle is
+    flexible as long as:
+      - The digits 1-9 specify a filled cell and the letter '.' (or the digit 0) an
+        empty cell. The digits (and '.') are process in a left-to-right and
+        top-to-bottom order, that is, the first digit (or '.') encountered specifies the
+        value of the top-left most cell, the second, the top-second-leftmost, ..., and
+        the last one the bottom-right-most cell.
       - There are exactly 81 cell values specified.
-    The routine returns a list of puzzles. Each puzzle is represented as a list of 81 digits (0-9).
+    The routine returns a list of puzzles. Each puzzle is represented as a list of 81
+    digits (0-9).
     """
     puzzles = []
     puzzle = []
@@ -39,23 +43,24 @@ def read_puzzles(name):
             puzzles.append(puzzle)
     return puzzles
 
+
 def write_puzzle_constraints(name, constraints):
     """
     Writes the constraints to a file in a format consistent with 'SimpleCSP' format.
     """
     with open(name, 'w') as f:
         for c in constraints:
-            f.write( '(' )
+            f.write('(')
             f.write(str(c[0]))
             f.write(',')
             f.write(str(c[1]))
             f.write(')\n')
-    return
 
 
 def write_puzzles_domains(name, puzzles):
     """
-    Writes the domains (for possibly multiple puzzle instances) to a file in a format consistent with 'SimpleCSP' format.
+    Writes the domains (for possibly multiple puzzle instances) to a file in a format
+    consistent with 'SimpleCSP' format.
     """
     with open(name, 'w') as f:
         first_puzzle = True
@@ -67,14 +72,14 @@ def write_puzzles_domains(name, puzzles):
             for domain in puzzle:
                 f.write(str(domain))
                 f.write('\n')
-    return
 
 
 def generate_domains(puzzles):
     """
     Generate variable domains from the puzzle instances.
-    Returns a list. Each element in the list is a list of sets with proper variable domain values
-    (where the first set is the domain for variable 0, the second for variable 1 ect.)
+    Returns a list. Each element in the list is a list of sets with proper variable
+    domain values (where the first set is the domain for variable 0, the second for
+    variable 1 ect.)
     """
 
     # ********** YOU IMPLEMENT THIS **********
@@ -84,11 +89,12 @@ def generate_domains(puzzles):
 
 def generate_constraints():
     """
-    The routine returns a list of non-equal constraints representing which pairs of cells cannot take the same
-    value. Each constraint is represented as a tuple of two integer values, e.g. as (0,1). The value 0 refers
-    to the top-left-most cell, the value 1 the top-2nd-left-most, ..., and the value 80 the bottom-right-most cell.
-    As non-equal constraints are symmetrical there is no need to generate the symmetri, e.g., generate (0,1) but
-    not (1,0).
+    The routine returns a list of non-equal constraints representing which pairs of
+    cells cannot take the same value. Each constraint is represented as a tuple of two
+    integer values, e.g. as (0,1). The value 0 refers to the top-left-most cell, the
+    value 1 the top-2nd-left-most, ..., and the value 80 the bottom-right-most cell. As
+    non-equal constraints are symmetrical there is no need to generate the symmetri,
+    e.g., generate (0,1) but not (1,0).
     """
 
     # ********** YOU IMPLEMENT THIS **********
@@ -96,17 +102,19 @@ def generate_constraints():
     return []
 
 
-#
-# Main program
-#
-#   Usage:  sudoku [filename]
-#     Reads Sudoku puzzle-instances from a file and outputs 'SimpleCSP' compatible constraints- and domains files,
-#     called 'filename_cst.txt' and 'filename_dom.txt', respectively.
-#
 def main():
+    """
+    'soduko.txt' is the default expected input file name, but it can be overwritten by
+    specifying an alternative name as a command-line argument.
+
+    Usage:  sudoku [filename]
+    Reads Sudoku puzzle-instances from a file and outputs 'SimpleCSP' compatible
+    constraints- and domains files, called 'filename_cst.txt' and 'filename_dom.txt',
+    respectively.
+    """
     name = 'sudoku'
-    input_puzzle_file = name + '.txt' # 'soduko.txt' is the default expected input file name, but it can be overwritten
-    if len(sys.argv) == 2:            # by specifying an alternative name as a command-line argument.
+    input_puzzle_file = name + '.txt'
+    if len(sys.argv) == 2:
         input_puzzle_file = sys.argv[1]
         name = Path(input_puzzle_file).stem
         assert len(name) > 0
@@ -116,14 +124,14 @@ def main():
     print('Processing puzzles from file', input_puzzle_file)
     puzzles = read_puzzles(input_puzzle_file)
     print('Read in', len(puzzles), 'Sudoku puzzle instances.')
-    exit(0) # TODO: REMOVE
-    print( 'Generating and writing domains to file', output_domains_file)
-    domains = generate_domains(puzzles)
-    write_puzzles_domains( name + "_dom.txt", domains)
 
-    print( 'Generating and writing constraints to file', output_constraints_file)
-    constraints = generate_constraints()                           # Generate and write out the constraints for Sudoku.
-    write_puzzle_constraints(output_constraints_file, constraints) # Note, the constraints are independent of the instances.
+    print('Generating and writing domains to file', output_domains_file)
+    domains = generate_domains(puzzles)
+    write_puzzles_domains(name + "_dom.txt", domains)
+
+    print('Generating and writing constraints to file', output_constraints_file)
+    constraints = generate_constraints()
+    write_puzzle_constraints(output_constraints_file, constraints)
 
 
 if __name__ == "__main__":
